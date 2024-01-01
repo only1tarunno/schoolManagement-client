955482f30a153d6a43024a2b1dce24c6cc990e47
only1tarunno <tarunno281@gmail.com>
2024-01-01 13:15:53 +0600

    section title add

---
+++ src/components/shared/SectionTitle.jsx
@@ -0,0 +1,12 @@
+const SectionTitle = ({ title, subtitle }) => {
+  return (
+    <div className="text-center">
+      <span className="text-[#0ab99d] bg-[#0ab99c33] uppercase px-5 py-1 rounded mb-5 inline-block">
+        {subtitle}
+      </span>
+      <h2 className="text-[#0e2a46] font-bold text-3xl lg:text-5xl">{title}</h2>
+    </div>
+  );
+};
+
+export default SectionTitle;
---
+++ src/pages/home/Category.jsx
@@ -0,0 +1,17 @@
+import Container from "../../components/shared/Container";
+import SectionTitle from "../../components/shared/SectionTitle";
+
+const Category = () => {
+  return (
+    <div>
+      <Container>
+        <SectionTitle
+          title={"Browse By Categories"}
+          subtitle={"CATEGORIES"}
+        ></SectionTitle>
+      </Container>
+    </div>
+  );
+};
+
+export default Category;

39849a5c9c555264e32789c59e6238980eff3d94
only1tarunno <tarunno281@gmail.com>
2024-01-02 13:29:50 +0600

    react course card add

--- src/pages/home/FeturedCourse.jsx
+++ src/pages/home/FeturedCourse.jsx
@@ -1,5 +1,8 @@
 import Container from "../../components/shared/Container";
 import SectionTitle from "../../components/shared/SectionTitle";
+import course1 from "../../assets/course-1.jpg";
+import course2 from "../../assets/course-2.jpg";
+import course3 from "../../assets/course-3.jpg";
 
 const FeturedCourse = () => {
   return (
@@ -9,6 +12,20 @@ const FeturedCourse = () => {
           subtitle={"TOP POPULAR COURSE"}
           title={"Histudy Course Student Can Join With Us"}
         ></SectionTitle>
+        <div>
+          <div className="card w-96 bg-base-100 shadow-xl rounded-md">
+            <figure className="px-5 pt-5">
+              <img src={course1} alt="" className="rounded-lg" />
+            </figure>
+            <div className="card-body px-5">
+              <h2 className="card-title">Shoes!</h2>
+              <p>If a dog chews shoes whose shoes does he choose?</p>
+              <div className="card-actions">
+                <button className="btn btn-primary">Buy Now</button>
+              </div>
+            </div>
+          </div>
+        </div>
       </Container>
     </div>
   );

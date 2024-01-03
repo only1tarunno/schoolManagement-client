f916c5061b0e50ee35b37187d8dd59d74580401f
only1tarunno <tarunno281@gmail.com>
2024-01-03 13:53:15 +0600

    add details in about us section

--- src/pages/home/About.jsx
+++ src/pages/home/About.jsx
@@ -17,7 +17,39 @@ const About = () => {
               <img src={img3} className="rounded-lg w-full" alt="" />
             </div>
           </div>
-          <div></div>
+          <div className="w-full md:w-[48%] space-y-5">
+            <span className="text-[#0ab99d] bg-[#0ab99c33] uppercase px-5 py-1 rounded  inline-block">
+              OUR ABOUT US
+            </span>
+            <h2 className="text-[#0e2a46] font-bold text-3xl lg:text-5xl w-full md:max-w-2xl">
+              Learn & Grow Your Skills From Anywhere
+            </h2>
+            <p className="text-base lg:text-lg">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris..
+            </p>
+            <div className="flex justify-between gap-5 flex-col lg:flex-row">
+              <div className="flex-1">
+                <h2 className="text-[#0e2a46] font-bold text-lg">
+                  FLEXIBLE CLASSES
+                </h2>
+                <p className="text-base lg:text-lg">
+                  Suspendisse ultrice gravida dictum fusce placerat ultricies
+                  integer quis auctor elit sed vulputate mi sit.
+                </p>
+              </div>
+              <div className="flex-1">
+                <h2 className="text-[#0e2a46] font-bold text-lg">
+                  FLEXIBLE CLASSES
+                </h2>
+                <p className="text-base lg:text-lg">
+                  Suspendisse ultrice gravida dictum fusce placerat ultricies
+                  integer quis auctor elit sed vulputate mi sit.
+                </p>
+              </div>
+            </div>
+          </div>
         </div>
       </Container>
     </div>

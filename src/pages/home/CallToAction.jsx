08a8a2079babf597a02a01aaccdf9c22dd4d6ffc
only1tarunno <tarunno281@gmail.com>
2024-01-04 14:42:05 +0600

    cto section done

--- src/pages/home/CallToAction.jsx
+++ src/pages/home/CallToAction.jsx
@@ -8,14 +8,14 @@ const CallToAction = () => {
         backgroundImage: `linear-gradient(rgba(14, 42, 70, 0.8), rgba(14, 42, 70, 0.8)),url(${img})`,
       }}
     >
-      <div className=" z-30 text-center space-y-2">
-        <h3>Join Our New Session</h3>
-        <h2 className="font-bold text-3xl max-w-3xl capitalize text-white ">
+      <div className=" z-30 text-center space-y-4 text-white font-bold">
+        <h3 className="text-lg md:text-2xl">Join Our New Session</h3>
+        <h2 className=" text-3xl md:text-4xl lg:text-5xl">
           Call To Enroll Your Child
         </h2>
-        <h2>+880 1569130064</h2>
+        <h2 className=" text-3xl md:text-4xl lg:text-5xl">(+880) 1569130064</h2>
         <button className="text-base font-bold shadow-none  text-white btn bg-[#0ab99d] border-none rounded hover:bg-[#52e7ce] focus:outline-none">
-          More About Us
+          Join With Us
         </button>
       </div>
     </div>

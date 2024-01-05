3561405acd8f344e89e197790976f663fb609a3e
only1tarunno <tarunno281@gmail.com>
2024-01-05 17:58:02 +0600

    add img in why us section

--- src/pages/home/WhyUs.jsx
+++ src/pages/home/WhyUs.jsx
@@ -1,47 +1,28 @@
 import Container from "../../components/shared/Container";
+import img1 from "../../assets/choose.jpg";
 
 const WhyUs = () => {
   return (
-    <div>
+    <div className="py-10 lg:py-16">
       <Container>
-        <div>
+        <div className="flex flex-col md:flex-row justify-between items-center">
           <div className="w-full md:w-[48%] space-y-5 text-center md:text-start">
             <span className="text-[#0ab99d] bg-[#0ab99c33] uppercase px-5 py-1 rounded  inline-block">
-              OUR ABOUT US
+              WHY CHOOSE US
             </span>
             <h2 className="text-[#0e2a46] font-bold text-3xl lg:text-5xl w-full md:max-w-2xl">
-              Learn & Grow Your Skills From Anywhere
+              Creating A Community Of Life Long Learners
             </h2>
             <p className="text-base lg:text-lg">
               Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
               eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
               enim ad minim veniam, quis nostrud exercitation ullamco laboris..
             </p>
-            <div className="flex justify-between gap-5 flex-col lg:flex-row">
-              <div className="flex-1">
-                <h2 className="text-[#0e2a46] font-bold text-lg">
-                  FLEXIBLE CLASSES
-                </h2>
-                <p className="text-base lg:text-lg">
-                  Suspendisse ultrice gravida dictum fusce placerat ultricies
-                  integer quis auctor elit sed vulputate mi sit.
-                </p>
-              </div>
-              <div className="flex-1">
-                <h2 className="text-[#0e2a46] font-bold text-lg">
-                  FLEXIBLE CLASSES
-                </h2>
-                <p className="text-base lg:text-lg">
-                  Suspendisse ultrice gravida dictum fusce placerat ultricies
-                  integer quis auctor elit sed vulputate mi sit.
-                </p>
-              </div>
-            </div>
-            <button className="text-base font-bold shadow-none  text-white btn bg-[#0ab99d] border-none rounded hover:bg-[#52e7ce] focus:outline-none">
-              More About Us
-            </button>
+            <div></div>
           </div>
-          <div></div>
+          <div className="w-full md:w-[40%]">
+            <img src={img1} className="w-full" alt="" />
+          </div>
         </div>
       </Container>
     </div>

21c4f80a37c52089c53ca114fcc407e65c57e050
only1tarunno <tarunno281@gmail.com>
2024-01-07 13:24:52 +0600

    teammember section done

--- src/pages/home/TeamMemberSingleImage.jsx
+++ src/pages/home/TeamMemberSingleImage.jsx
@@ -13,7 +13,7 @@ const TeamMemberSingleImage = ({ img1 }) => {
     setHovered(false);
   };
   return (
-    <div className="relative w-[360px] h-[360px] border-4 border-[#0ab99d] overflow-hidden">
+    <div className="relative w-[360px] teamImg h-[360px] border-4 border-[#0ab99d] overflow-hidden cursor-pointer">
       <img
         src={img1}
         className={` transition-transform duration-1000 w-full h-full object-cover  ${
@@ -23,7 +23,7 @@ const TeamMemberSingleImage = ({ img1 }) => {
         onMouseOver={handleMouseOver}
         onMouseOut={handleMouseOut}
       />
-      <div className="flex justify-between items-center bg-white absolute w-4/5 left-1/2 -translate-x-1/2 bottom-3 p-4 rounded-md">
+      <div className="flex justify-between items-center bg-white absolute w-4/5 left-1/2 -translate-x-1/2 bottom-3 p-4 rounded-md z-10">
         <div>
           <h3 className="text-[#0e2a46] text-xl font-bold">Esther Howard</h3>
           <p className="text-sm text-[#0ab99d] font-semibold">

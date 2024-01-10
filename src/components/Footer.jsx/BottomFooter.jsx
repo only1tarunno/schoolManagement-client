44f98ae84acbe297f81c517c3c38ec4b10029ab7
only1tarunno <tarunno281@gmail.com>
2024-01-10 14:40:43 +0600

    cpyright add

--- src/components/Footer.jsx/BottomFooter.jsx
+++ src/components/Footer.jsx/BottomFooter.jsx
@@ -1,8 +1,10 @@
 const BottomFooter = () => {
   return (
-    <div className="footer footer-center p-4 bg-base-300 text-base-content">
+    <div className="footer footer-center p-4  text-base-content bg-[#222]">
       <aside>
-        <p>Copyright © 2024 - All right reserved by ACME Industries Ltd</p>
+        <p>
+          Copyright © 2024 <a href="/">Educate</a> || All Rights Reserved
+        </p>
       </aside>
     </div>
   );

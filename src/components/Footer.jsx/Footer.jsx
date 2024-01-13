3f1107227b837c91f0e4577670bed77ea1249b1b
only1tarunno <tarunno281@gmail.com>
2024-01-13 18:05:23 +0600

    footer complete

--- src/components/Footer.jsx/Footer.jsx
+++ src/components/Footer.jsx/Footer.jsx
@@ -6,32 +6,26 @@ const Footer = () => {
     <div className="bg-black">
       <Container>
         <footer className="footer p-10 text-white">
-          <div className="w-full lg:w-2/6">
+          <div className="">
             <img src={logo} alt="" />
-            <p>
+            <p className="text-lg font-medium">
               Interdum velit laoreet id donec ultrices tincidunt arcu. Tincidunt
               tortor aliquam nulla facilisi cras fermentum odio eu.
             </p>
           </div>
-          <nav>
+          <nav className="">
             <header className="footer-title">Services</header>
-            <a className="link link-hover">Branding</a>
-            <a className="link link-hover">Design</a>
-            <a className="link link-hover">Marketing</a>
-            <a className="link link-hover">Advertisement</a>
+            <a className="link link-hover">Web Development</a>
+            <a className="link link-hover">UI/UX Design</a>
+            <a className="link link-hover">Management</a>
+            <a className="link link-hover">Digital Marketing</a>
           </nav>
-          <nav>
+          <nav className="">
             <header className="footer-title">Company</header>
             <a className="link link-hover">About us</a>
             <a className="link link-hover">Contact</a>
             <a className="link link-hover">Jobs</a>
             <a className="link link-hover">Press kit</a>
-          </nav>
-          <nav>
-            <header className="footer-title">Legal</header>
-            <a className="link link-hover">Terms of use</a>
-            <a className="link link-hover">Privacy policy</a>
-            <a className="link link-hover">Cookie policy</a>
           </nav>
         </footer>
       </Container>

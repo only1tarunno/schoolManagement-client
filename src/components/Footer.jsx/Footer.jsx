a001495df2774f8d28574bc75e9ebbbee40c265e
only1tarunno <tarunno281@gmail.com>
2024-01-13 18:11:33 +0600

    kk

--- src/components/Footer.jsx/Footer.jsx
+++ src/components/Footer.jsx/Footer.jsx
@@ -25,7 +25,6 @@ const Footer = () => {
             <a className="link link-hover">About us</a>
             <a className="link link-hover">Contact</a>
             <a className="link link-hover">Jobs</a>
-            <a className="link link-hover">Press kit</a>
           </nav>
         </footer>
       </Container>
